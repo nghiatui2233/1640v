e9af0b0b2a93ad39607a990c16b9f1c32f8ee25a
nghiatui2233 <91666125+nghiatui2233@users.noreply.github.com>
2023-04-14 15:35:24 +0700

    db

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "idea_management"; 
+$host = "us-cdbr-east-06.cleardb.net";
+$username = "bdea88aeb912e6";
+$password = "61a9eddb";
+$database = "heroku_484bc174daa4b81"; 
 
 $conn = mysqli_connect($host, $username, $password, $database);
 if (!$conn) {
