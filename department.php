d224017fa86f0a88155c5d7ab992bc0d238329dc
nghiatui2233 <nghiatui2233@gmail.com>
2023-04-15 15:51:30 +0700

    Update department.php

--- department.php
+++ department.php
@@ -10,7 +10,7 @@
 
   <form action=""></form>
   <b>Main Function:</b><br />
-  <button onclick="window.location='creareDepartment.php'" class="nv btn add-new" type="button" data-toggle="tooltip" data-placement="top" title="Add Post">
+  <button onclick="window.location='creareDepartment.php'" class="nv btn add-new" type="button" data-toggle="tooltip" data-placement="top" title="Add Department">
     <i class="fas fa-user-plus" aria-hidden="true"></i>
   </button>
 
