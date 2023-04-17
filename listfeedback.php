face6aba85a6ac01c381e6a93cc43d8c27cde990
nghiatui2233 <nghiatui2233@gmail.com>
2023-04-17 13:01:03 +0700

    Update listfeedback.php

--- listfeedback.php
+++ listfeedback.php
@@ -9,7 +9,7 @@
         margin: 50px auto;
         max-width: 800px;
         padding: 0 20px;
-        margin-top: 75px;
+        margin-top: 100px;
         background-color: #DDDDDD;
         border-radius: 15px;
     }
@@ -127,7 +127,7 @@
 
     .a {
         float: right;
-       margin-top: 25px;
+        margin-top: 25px;
     }
 
     .scroll-btn {
@@ -158,7 +158,7 @@ if (isset($_GET["function"]) == "del") {
         $id = $_GET["id"];
         $sql = "SELECT * FROM tbl_feedback";
         mysqli_query($conn, "DELETE FROM tbl_feedback WHERE feedback_Id='$id'");
-            echo '<meta http-equiv="refresh" content="0; '.$_SERVER['HTTP_REFERER'].'">';
+        echo '<meta http-equiv="refresh" content="0; ' . $_SERVER['HTTP_REFERER'] . '">';
     }
 }
 if (isset($_GET['id'])) {
@@ -178,28 +178,34 @@ ORDER BY tbl_feedback.likes DESC
 
             <body>
                 <div class="container">
-                    <a class="a" href="?&&function=del&&id=<?php echo $row["feedback_Id"]; ?>"><span style="color: red; " class="fas fa-trash" style="color:black"></span></a>
-                        <h1><strong>Post title: <?php echo $row['title']  ?></strong></h1>
-                        <form method="POST" enctype="multipart/form-data">
-                            <div class="half left cf">
-                                <input type="text" name="" id="input-name" readonly value="<?php echo $row['anonymous']; ?>" />
-                            </div>
-                            <textarea name="feedback" type="text" id="input-message" placeholder="Message" readonly><?php echo $row['feedback']; ?></textarea>
-                            <?php
-                            if ($row2["role"] == 1) {
-                            ?>
-                                <button class="button-download">
-                                    <a href="<?php echo $row['file_path']; ?>" download="<?php echo $row['file_name']; ?>" style="color: #fff;">Download file</a>
-                                </button>
-                            <?php
-                            }
-                            ?>
-                        </form>
-                        <br>
-                        <div class="actions">
-                            <button id="view-more" class="button" onclick="window.location='comment.php?id=<?php echo $row['feedback_Id']; ?>'">Comment</button>
-                            <button class="button"><i class='fas fa-solid fa-thumbs-up'></i>: <?php echo $row['likes'] ?></button>
+                    <?php
+                    if ($row2["role"] == 1) {
+                    ?>
+                        <a class="a" href="?&&function=del&&id=<?php echo $row["feedback_Id"]; ?>"><span style="color: red; " class="fas fa-trash"></span></a>
+                    <?php
+                    }
+                    ?>
+                    <h1><strong>Post title: <?php echo $row['title']  ?></strong></h1>
+                    <form method="POST" enctype="multipart/form-data">
+                        <div class="half left cf">
+                            <input type="text" name="" id="input-name" readonly value="<?php echo $row['anonymous']; ?>" />
                         </div>
+                        <textarea name="feedback" type="text" id="input-message" placeholder="Message" readonly><?php echo $row['feedback']; ?></textarea>
+                        <?php
+                        if ($row2["role"] == 1) {
+                        ?>
+                            <button class="button-download">
+                                <a href="<?php echo $row['file_path']; ?>" download="<?php echo $row['file_name']; ?>" style="color: #fff;">Download file</a>
+                            </button>
+                        <?php
+                        }
+                        ?>
+                    </form>
+                    <br>
+                    <div class="actions">
+                        <button id="view-more" class="button" onclick="window.location='comment.php?id=<?php echo $row['feedback_Id']; ?>'">Comment</button>
+                        <button class="button"><i class='fas fa-solid fa-thumbs-up'></i>: <?php echo $row['likes'] ?></button>
+                    </div>
                 </div>
             </body>
         <?php }
