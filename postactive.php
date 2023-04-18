2c20487bba2f4e18a8c0af47c9078a01d960094e
nghiatui2233 <nghiatui2233@gmail.com>
2023-04-18 17:02:46 +0700

    Update postactive.php

--- postactive.php
+++ postactive.php
@@ -1,11 +1,9 @@
 <?php
     include('config.php');
     $ids = array();
-    // $ids = implode(",",$_POST["id"]);
     $ids = $_POST["id"];
     
     
-    // $deactive = "UPDATE inf SET active=0 where n_id IN(".$ids.")";
     $deactive = "UPDATE tbl_post SET active=0 where post_Id= ".$ids." ";
     
     $result = mysqli_query($conn,$deactive);
