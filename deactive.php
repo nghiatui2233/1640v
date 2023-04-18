9f44cd09beb2395bff4a62577fb3ae84563ef2b6
nghiatui2233 <nghiatui2233@gmail.com>
2023-04-18 16:48:07 +0700

    Update deactive.php

--- deactive.php
+++ deactive.php
@@ -1,11 +1,9 @@
 <?php
     include('config.php');
     $ids = array();
-    // $ids = implode(",",$_POST["id"]);
     $ids = $_POST["id"];
     
     
-    // $deactive = "UPDATE inf SET active=0 where n_id IN(".$ids.")";
     $deactive = "UPDATE tbl_comment SET active=0 where comment_Id= ".$ids." ";
     
     $result = mysqli_query($conn,$deactive);
